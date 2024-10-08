4d5cc1450b0134977e974574267f8d4bdef0ab9d
Zakari Adamu <ojonimizakari@gmail.com>
2024-10-08 17:54:28 +0100

    add users wishlist functionalities

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,8 +1,61 @@
+"use client";
+import { useUser } from "@clerk/nextjs";
 import { Heart } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const ProductCard = ({ product }: { product: ProductType }) => {
+	const { user } = useUser();
+	const router = useRouter();
+
+	const [loading, setLoading] = useState(false);
+	const [signedInUser, setSignedInUser] = useState<UserType | null>(null);
+	const [isLiked, setIsLiked] = useState(false);
+
+	const getUser = async () => {
+		try {
+			setLoading(true);
+			const res = await fetch("/api/users");
+			const data = await res.json();
+			setSignedInUser(data);
+			setIsLiked(data.wishlist.includes(product._id));
+			setLoading(false);
+		} catch (error) {
+			console.log("[users_GET]", error);
+		}
+	};
+
+	useEffect(() => {
+		if (user) {
+			getUser();
+		}
+	}, [user]);
+
+	const handleLike = async (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
+		e.preventDefault();
+
+		try {
+			if (!user) {
+				router.push("/sign-in");
+				return;
+			} else {
+				setLoading(true);
+				const res = await fetch("/api/users/wishlist", {
+					method: "POST",
+					body: JSON.stringify({ productId: product._id }),
+				});
+
+				const updatedUser = await res.json();
+				setSignedInUser(updatedUser);
+				setIsLiked(updatedUser.wishlist.includes(product._id));
+			}
+		} catch (error) {
+			console.log("[wishlist_POST]", error);
+		}
+	};
+
 	return (
 		<Link
 			href={`/products/${product._id}`}
@@ -21,8 +74,8 @@ const ProductCard = ({ product }: { product: ProductType }) => {
 			</div>
 			<div className="flex justify-between items-center">
 				<p className="text-body-bold">${product.price}</p>
-				<button>
-					<Heart />
+				<button onClick={handleLike}>
+					<Heart fill={`${isLiked ? "red" : "white"}`} />
 				</button>
 			</div>
 		</Link>
