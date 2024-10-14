c10e10ffe5a04e1b80d45dbc2c4a5b6b987cfe96
Zakari Adamu <ojonimizakari@gmail.com>
2024-10-14 21:09:38 +0100

    implement search component

---
+++ app/(root)/search/[query]/page.tsx
@@ -0,0 +1,28 @@
+import ProductCard from "@/app/components/ProductCard";
+
+const SearchPage = async ({ params }: { params: { query: string } }) => {
+	const res = await fetch(
+		`${process.env.NEXT_PUBLIC_API_URL}/search/${params.query}`
+	);
+	const searchedProducts = await res.json();
+
+	return (
+		<div className="px-10 py-5">
+			<p className="text-heading3-bold my-10">
+				Search results for {params.query}
+			</p>
+			{!searchedProducts ||
+				(searchedProducts.length === 0 && (
+					<p className="text-body-bold my-5">No result found</p>
+				))}
+
+			<div className="flex flex-wrap justify-center gap-16">
+				{searchedProducts?.map((product: any) => (
+					<ProductCard key={product._id} product={product} />
+				))}
+			</div>
+		</div>
+	);
+};
+
+export default SearchPage;
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,51 +1,98 @@
 "use client";
 
 import { UserButton, useUser } from "@clerk/nextjs";
-import { CircleUserRound, Menu, ShoppingCart } from "lucide-react";
+import { CircleUserRound, Menu, Search, ShoppingCart } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import useCart from "../lib/hooks/useCart";
+import { useRouter } from "next/navigation";
 
 const Navbar = () => {
 	// get clerk authenticated user
 	const { user } = useUser();
 	const cart = useCart();
 	const [dropDownMenu, setDropDownMenu] = useState(false);
+	const [searchQuery, setSearchQuery] = useState("");
+	const router = useRouter();
 
 	return (
-		<div className="sticky top-0 z-10 py-2 px-10 flex justify-between items-center bg-[#fff]">
+		<div className="sticky top-0 z-10 py-2 px-10 flex gap-2 justify-between items-center bg-[#fff] max-sm:px-2 ">
 			<Link href="/">
 				<Image src="/logo-beta.png" alt="site logo" width={130} height={100} />
 			</Link>
-			<div className="">
-				<Link href="/">Home</Link>
+			<div className="flex gap-4 text-base-bold max-lg:hidden">
+				<Link href="/" className="hover:text-red-600">
+					Home
+				</Link>
+				<Link
+					href={user ? "/wishlist" : "/sign-in"}
+					className="hover:text-red-600"
+				>
+					Wishlist
+				</Link>
+				<Link
+					href={user ? "/orders" : "/sign-in"}
+					className="hover:text-red-600"
+				>
+					Orders
+				</Link>
 			</div>
+
+			{/* Search input */}
+			<div className="flex gap-3 border border-grey-2 px-3 py-1 items-center rounded-lg">
+				<input
+					className="outline-none max-sm:max-w-[120px]"
+					placeholder="Search.."
+					value={searchQuery}
+					onChange={(e) => setSearchQuery(e.target.value)}
+				/>
+				<button
+					disabled={searchQuery === ""}
+					onClick={() => router.push(`/search/${searchQuery}`)}
+				>
+					<Search className="cursor-pointer h-4 w-4 hover:text-red-600" />
+				</button>
+			</div>
+
 			<div className="relative flex gap-3 items-center">
 				<Link
 					href="/cart"
-					className="flex items-center gap-3 border rounded-lg px-2 py-1 hover:bg-black hover:text-white"
+					className="flex items-center gap-3 border rounded-lg px-2 py-1 hover:bg-black hover:text-white max-md:hidden"
 				>
 					<ShoppingCart />
 					<p className="text-base-bold">Cart ({cart.cartItems.length})</p>
 				</Link>
-				{user && (
-					<>
-						<Menu
-							className="cursor-pointer"
-							onClick={() => setDropDownMenu(!dropDownMenu)}
-						/>
-						{dropDownMenu && (
-							<div className="absolute top-10 right-5 flex flex-col gap-2 p-3 rounded-lg border bg-white text-base-bold">
-								<Link href="/wishlist" className="hover:text-red-1">
-									Wishlist
-								</Link>
-								<Link href="/orders" className="hover:text-red-1">
-									Orders
-								</Link>
-							</div>
-						)}
-					</>
+
+				<Menu
+					className="cursor-pointer lg:hidden"
+					onClick={() => setDropDownMenu(!dropDownMenu)}
+				/>
+				{dropDownMenu && (
+					<div className="absolute top-12 right-5 flex flex-col gap-4 p-3 rounded-lg border bg-white text-base-bold lg:hidden">
+						<Link href="/" className="hover:text-red-600">
+							Home
+						</Link>
+						<Link
+							href={user ? "/wishlist" : "/sign-in"}
+							className="hover:text-red-600"
+						>
+							Wishlist
+						</Link>
+						<Link
+							href={user ? "/orders" : "/sign-in"}
+							className="hover:text-red-600"
+						>
+							Orders
+						</Link>
+						<Link
+							href="/cart"
+							className="flex items-center gap-3 border rounded-lg px-2 py-1 hover:bg-black hover:text-white"
+						>
+							<ShoppingCart />
+							<p className="text-base-bold">Cart ({cart.cartItems.length})</p>
+						</Link>
+					</div>
 				)}
 
 				{user ? (
